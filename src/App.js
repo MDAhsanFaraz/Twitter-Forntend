0dd6b541563f584e714d04f584d28202e90bcf10
Faraz <faraz.com>
2024-02-08 23:10:27 +0530

    Added TweetList Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
-import Tweet from "./components/Tweet";
+import TweetList from "./components/TweetList";
+
+const initialDummyTweets = [
+  { id: 0, content: "we have new twitter called threads ", likeCount: 90 },
+  { id: 1, content: "Farazzzzzz", likeCount: 10 },
+  { id: 2, content: "DAmmmmmmmmmmmmmmmm", likeCount: 20 },
+];
 
 function App() {
   return (
     <div>
-      <Tweet content={"First Tweet"} likeCount={20} />
+      <TweetList tweets={initialDummyTweets} />
     </div>
   );
 }
