2c6c5bd32ad6a10ed5f258b7e265386b0ea5e945
Faraz <faraz.com>
2024-02-10 10:05:01 +0530

    added tweets in the dependency array of use callback

--- src/components/Twitter.jsx
+++ src/components/Twitter.jsx
@@ -21,39 +21,45 @@ const initialDummyTweets = [
 const MemoisedTweet = memo(AddTweet);
 function Twitter() {
   const [tweets, setTweets] = useState(initialDummyTweets); //creating a state component to manage the state of tweets
-  const handleAddTweet = useCallback((text) => {
-    //this function will change the state and add new tweets inside tweets array
-    let nextId = tweets.length > 0 ? tweets[tweets.length - 1].id + 1 : 0;
-    setTweets([
-      ...tweets,
-      {
-        content: text,
-        likeCount: Math.floor(Math.random() * 10),
-        id: nextId,
-        createdAt: new Date(),
-      },
-    ]);
-  }, []);
+  const handleAddTweet = useCallback(
+    (text) => {
+      //this function will change the state and add new tweets inside tweets array
+      let nextId = tweets.length > 0 ? tweets[tweets.length - 1].id + 1 : 0;
+      setTweets([
+        ...tweets,
+        {
+          content: text,
+          likeCount: Math.floor(Math.random() * 10),
+          id: nextId,
+          createdAt: new Date(),
+        },
+      ]);
+    },
+    [tweets]
+  );
 
-  const handleEditTweet = useCallback((tweet) => {
-    //we will recieve updated tweet obj with same tweet id
-    setTweets(
-      tweets.map((currentTweet) => {
-        //using map function as soon we find tweet obj with same id inside tweet array
-        // we replace in the if() by returning the new tweet obj else we keep the other tweets obj same by returning
-        // currentTweet
-        if (currentTweet.id === tweet.id) {
-          return tweet;
-        } else {
-          return currentTweet;
-        }
-      })
-    );
-  }, []);
+  const handleEditTweet = useCallback(
+    (tweet) => {
+      //we will recieve updated tweet obj with same tweet id
+      setTweets(
+        tweets.map((currentTweet) => {
+          //using map function as soon we find tweet obj with same id inside tweet array
+          // we replace in the if() by returning the new tweet obj else we keep the other tweets obj same by returning
+          // currentTweet
+          if (currentTweet.id === tweet.id) {
+            return tweet;
+          } else {
+            return currentTweet;
+          }
+        })
+      );
+    },
+    [tweets]
+  );
   const sortTweets = useCallback(() => {
     tweets.sort((t1, t2) => t2.createdAt.getTime() - t1.createdAt.getTime());
     setTweets([...tweets]);
-  });
+  }, [tweets]);
   return (
     <>
       <MemoisedTweet onAddTweet={handleAddTweet} />
